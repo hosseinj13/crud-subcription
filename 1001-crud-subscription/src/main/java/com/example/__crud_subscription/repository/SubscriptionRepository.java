017ae78b20686f2c41cf5985fe941eafde8a18ea
hossein javadi <hosseinj13@yahoo.com>
2024-12-23 14:52:46 +0330

    Create SubscriptionRepository.java with JpaRepository.

---
+++ 1001-crud-subscription/src/main/java/com/example/__crud_subscription/repository/SubscriptionRepository.java
@@ -0,0 +1,9 @@
+package com.example.__crud_subscription.repository;
+
+import com.example.__crud_subscription.model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
+}
